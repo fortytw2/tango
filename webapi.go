8cd742bf37dae2bbd5d635ee4b23af38f17d741f
fortytw2 <fortytw2@gmail.com>
2015-10-11 18:44:56 +0200

    update godoc

--- webapi.go
+++ webapi.go
@@ -1,9 +1,13 @@
 package tango
 
+// WebAPI just wraps the parsing functionality found in requests/
+// in an easy to use API
 type WebAPI struct {
 	key string
 }
 
+// NewWebAPI creates a new instance of WebAPI, with the given steam development
+// key held internally. http://steamcommunity.com/dev/registerkey
 func NewWebAPI(key string) *WebAPI {
 	return &WebAPI{
 		key: key,
